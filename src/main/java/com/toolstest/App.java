a5be5338494eb201a12626e61f9c1fee0a1f8e47
Emilio López <milio81@gmail.com>
2019-09-19 20:29:53 +0200

    Fix PMD rule

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -4,25 +4,17 @@ package com.toolstest;
  * Hello world!
  */
 public class App {
-    
+
     private static String name;
 
     public static void main(String[] args) {
-        //TODO hacer algo con este proyecto
+        // TODO hacer algo con este proyecto
         name = "Fer";
         System.out.println(name);
     }
-    
-    public void metodo()
-    {
-        if(true){
-            if(true){
-                if(true){
-                    if(true){
-                        System.out.println(name);
-                    }
-                }
-            }    
-        }
+
+    public void metodo() {
+
+        System.out.println(name);
     }
 }
